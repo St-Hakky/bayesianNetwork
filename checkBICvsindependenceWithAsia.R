0668a76e2da9de93bc24e37010a28318d35a5bae
StHakky <hakky25133638@gmail.com>
2015-12-29 10:04:42 +0900

    独立とBICの対応関係のデータをリスト形式で保存する処理を追記

--- checkBICvsindependenceWithAsia.R
+++ checkBICvsindependenceWithAsia.R
@@ -1,6 +1,26 @@
 library(bnlearn)
 library(dplyr)
 
+printResultbyLine <- function(tp,fp,fn,tn){
+  print (paste("independence is true and BIC score is increase : ", tp, "(", 100*tp/(tp + fp + fn + tn)," parcent)"))
+  print (paste("independence is true and BIC score is decrease : ", fp, "(", 100*fp/(tp + fp + fn + tn)," parcent)"))
+  print (paste("independence is false and BIC score is increase : ", fn, "(", 100*fn/(tp + fp + fn + tn)," parcent)"))
+  print (paste("independence is false and BIC score is decrease : ", tn, "(", 100*tn/(tp + fp + fn + tn)," parcent)"))
+}
+
+printResultbyTable <- function(tp,fp,fn,tn,colnames = c("independence", "dependence"),rownames = c("increase", "decrease")){
+  result.table = matrix(0,2,2)
+  result.table[1,] = c(tp, fp)
+  result.table[2,] = c(fn, tn)
+  colnames(result.table) = colnames
+  rownames(result.table) = rownames
+  print (result.table)
+  result.table[1,] = c(100*tp/(tp + fp + fn + tn), 100*fp/(tp + fp + fn + tn))
+  result.table[2,] = c(100*fn/(tp + fp + fn + tn), 100*tn/(tp + fp + fn + tn))
+  print (result.table)
+}
+
+
 getScore <- function(type, a.sum.i, b.sum.i,c.sum.i,d.sum.i){
   if(type == "bic" || type == "aic"){
     # dataFrame
@@ -99,15 +119,95 @@ getSampleData <- function(try.size, data, data.names,sample.data.size){
   return(list(sample.table.data, sample.row.data))  
 }
 
-main <- function(data, sample.data.size = 100, try.size = 2){
+getDataFrame <- function(data){
+  a = data[,1]
+  b = data[,2]
+  data = data.frame(A=a, B=b)
+  return(data)
+}
+
+getScore <- function(data, type="bic"){
+  data = getDataFrame(data)
+  data[,1] = as.factor(data[,1])
+  data[,2] = as.factor(data[,2])
+  
+  # pre.score
+  res = empty.graph(colnames(data))
+  pre.score = -2 * score(res, data, type=type)
+  
+  # pro.score from A to B
+  res = set.arc(res,"A","B")
+  pro.score.fromAtoB = -2 * score(res, data, type=type)
+  
+  # pro.score from B to A
+  res = empty.graph(names(data))
+  res = set.arc(res,"B","A")
+  pro.score.fromBtoA = -2 * score(res, data, type=type)
+  
+  if(pro.score.fromAtoB < pro.score.fromBtoA){
+    pro.score = pro.score.fromBtoA
+  }else{
+    pro.score = pro.score.fromAtoB
+  }
+  return (list(pre.score, pro.score))
+  
+}
+
+main <- function(data, sample.data.size.vec = c(10), try.size = 10){
   data.names = names(data)
   data.length = length(data[, data.names[1]])
-  sample.data = getSampleData(try.size, data,data.names,sample.data.size)
-  sample.table.data = sample.data[[1]]
-  sample.row.data = sample.data[[2]]
-  print(sample.table.data[[28]][[1]])
+  independence.inc = list()
+  independence.dec = list()
+  dependence.inc = list()
+  dependence.dec = list()
+  sample.data.index = 1
+  for(sample.data.size in sample.data.size.vec){
+    sample.data = getSampleData(try.size, data,data.names,sample.data.size)
+    sample.table.data = sample.data[[1]]
+    sample.row.data = sample.data[[2]]
+    for(i in 1:length(sample.row.data)){
+      tp.val = 0
+      tn.val = 0
+      fp.val = 0
+      fn.val = 0
+      for(j in 1:length(sample.row.data[[i]])){
+        print(paste("i : ", i, "j : ", j))
+        score = getScore(sample.row.data[[i]][[j]])
+        pre.score = score[[1]]
+        pro.score = score[[2]]
+        res.fisher = fisher.test(sample.table.data[[i]][[j]])
+        if(pre.score < pro.score && res.fisher$p.value > 0.05){        # TP
+          tp.val = tp.val + 1
+        }else if(pre.score >= pro.score && res.fisher$p.value > 0.05){ # FP
+          fp.val = fp.val + 1 
+        }else if(pre.score < pro.score && res.fisher$p.value <= 0.05){ # FN
+          fn.val = fn.val + 1
+        }else{                                                        # TN
+          tn.val = tn.val + 1
+        }
+      }
+      if(sample.data.index == 1){
+        independence.inc = c(independence.inc, list(list()))
+        independence.dec = c(independence.dec, list(list()))
+        dependence.inc = c(dependence.inc, list(list()))
+        dependence.dec = c(dependence.dec, list(list()))
+        independence.inc[[i]] = c(independence.inc[[i]], list(tp.val/try.size))
+        independence.dec[[i]] = c(independence.dec[[i]], list(fp.val/try.size))
+        dependence.inc[[i]] = c(dependence.inc[[i]], list(fn.val/try.size))
+        dependence.dec[[i]] = c(dependence.dec[[i]], list(tn.val/try.size))
+      }else{
+        independence.inc[[i]] = c(independence.inc[[i]], list(tp.val/try.size))
+        independence.dec[[i]] = c(independence.dec[[i]], list(fp.val/try.size))
+        dependence.inc[[i]] = c(dependence.inc[[i]], list(fn.val/try.size))
+        dependence.dec[[i]] = c(dependence.dec[[i]], list(tn.val/try.size))
+      }
+      printResultbyTable(tp.val, fp.val, fn.val,tn.val)
+      printResultbyLine(tp.val, fp.val, fn.val,tn.val)
+    }
+    sample.data.index = sample.data.index + 1
+  }
 }
 
 data(asia)
-main(asia, 200)
+main(asia, c(10,20))
 
