66279326ea1979d7fdf1544b9a4015fc4ee1e8b1
StHakky <hakky25133638@gmail.com>
2016-01-06 14:55:57 +0900

    スコアの修正。フィッシャーの正確確率検定の修正。また、データの取り直し。

--- checkBICvsIndependence.R
+++ checkBICvsIndependence.R
@@ -7,30 +7,34 @@ plotData <- function(x,y,xlab,ylab,col=col){
 }
 
 plotResult <- function(x, y, p.value, xlab, ylab,title.main,p.line.col="blue", BIC.eq.col="red"){
-  plot(x,y,xlab=xlab, ylab=ylab, ylim=c(0,1))
+  plot(x,y,xlab=xlab, ylab=ylab)
   par(new=T)
   title(title.main)
-  abline(h = p.value, col=p.line.col)
+  abline(h = p.value/2, col=p.line.col)
+  abline(h = 1-p.value/2, col=p.line.col)
   abline(v = 0, col=BIC.eq.col)
   par(new=F)
 }
 
-printResultbyLine <- function(tp,fp,fn,tn){
-  print (paste("independence is true and BIC score is increase : ", tp, "(", 100*tp/(tp + fp + fn + tn)," parcent)"))
-  print (paste("independence is true and BIC score is decrease : ", fp, "(", 100*fp/(tp + fp + fn + tn)," parcent)"))
-  print (paste("independence is false and BIC score is increase : ", fn, "(", 100*fn/(tp + fp + fn + tn)," parcent)"))
-  print (paste("independence is false and BIC score is decrease : ", tn, "(", 100*tn/(tp + fp + fn + tn)," parcent)"))
+printResultbyLine <- function(independence.betterScore, independence.warseScore, dependence.betterScore, dependence.warseScore){
+  total = independence.betterScore + independence.warseScore + dependence.betterScore + dependence.warseScore
+  print (paste("independence is true and BIC score is better : ", independence.betterScore, "(", 100*independence.betterScore/total," parcent)"))
+  print (paste("independence is true and BIC score is warse : ", independence.warseScore, "(", 100*independence.warseScore/total," parcent)"))
+  print (paste("independence is false and BIC score is better : ", dependence.betterScore, "(", 100*dependence.betterScore/total," parcent)"))
+  print (paste("independence is false and BIC score is warse : ", dependence.warseScore, "(", 100*dependence.warseScore/total," parcent)"))
 }
 
-printResultbyTable <- function(tp,fp,fn,tn,colnames = c("independence", "dependence"),rownames = c("increase", "decrease")){
+
+printResultbyTable <- function(independence.betterScore,independence.warseScore,dependence.betterScore,dependence.warseScore,colnames = c("independence", "dependence"),rownames = c("better score", "warse score")){
+  total = independence.betterScore + independence.warseScore + dependence.betterScore + dependence.warseScore
   result.table = matrix(0,2,2)
-  result.table[1,] = c(tp, fp)
-  result.table[2,] = c(fn, tn)
+  result.table[1,] = c(independence.betterScore, independence.warseScore)
+  result.table[2,] = c(dependence.betterScore, dependence.warseScore)
   colnames(result.table) = colnames
   rownames(result.table) = rownames
   print (result.table)
-  result.table[1,] = c(100*tp/(tp + fp + fn + tn), 100*fp/(tp + fp + fn + tn))
-  result.table[2,] = c(100*fn/(tp + fp + fn + tn), 100*tn/(tp + fp + fn + tn))
+  result.table[1,] = c(100*independence.betterScore/total, 100*independence.warseScore/total)
+  result.table[2,] = c(100*dependence.betterScore/total, 100*dependence.warseScore/total)
   print (result.table)
 }
 
@@ -61,10 +65,11 @@ getScore <- function(type, a.sum.i, b.sum.i,c.sum.i,d.sum.i){
     res = set.arc(res,"B","A")
     pro.score.fromBtoA = -2 * score(res, data, type=type)
     
+    # the smaller score is better
     if(pro.score.fromAtoB < pro.score.fromBtoA){
-      pro.score = pro.score.fromBtoA
-    }else{
       pro.score = pro.score.fromAtoB
+    }else{
+      pro.score = pro.score.fromBtoA
     }
   }
   return (list(pre.score, pro.score))
@@ -109,10 +114,10 @@ main <- function(data.size.num.vec = c(10), p.value= 0.05){
     b.sum = table.value[[2]]
     c.sum = table.value[[3]]
     d.sum = table.value[[4]]
-    tp.val = 0
-    tn.val = 0
-    fp.val = 0
-    fn.val = 0
+    independence.betterScore = 0
+    independence.warseScore = 0
+    dependence.betterScore = 0
+    dependence.warseScore = 0
     for(i in 1:length(a.sum)){
       
       if(a.sum[i] != 0 && b.sum[i] != 0 && c.sum[i] != 0 && d.sum[i]){
@@ -126,51 +131,61 @@ main <- function(data.size.num.vec = c(10), p.value= 0.05){
         pre.score = score[[1]]
         pro.score = score[[2]]
         if(plot.index == 0){
-          x = c(pro.score - pre.score)
+          x = c(pre.score - pro.score)
           y = c(res.fisher$p.value)
         }else{
-          x = append(x, pro.score - pre.score)
+          x = append(x, pre.score - pro.score)
           y = append(y, res.fisher$p.value)
         }
         plot.index = plot.index + 1
         
         
-        if(pre.score < pro.score && res.fisher$p.value > 0.05){        # TP
-          tp.val = tp.val + 1
-        }else if(pre.score >= pro.score && res.fisher$p.value > 0.05){ # FP
-          fp.val = fp.val + 1 
-        }else if(pre.score < pro.score && res.fisher$p.value <= 0.05){ # FN
-          fn.val = fn.val + 1
-        }else{                                                        # TN
-          tn.val = tn.val + 1
+        if((pre.score - pro.score < 0) && res.fisher$p.value > 0.025 && res.fisher$p.value < 0.975){        # 
+          independence.warseScore = independence.warseScore + 1
+        }else if((pre.score - pro.score >= 0) && res.fisher$p.value > 0.025  && res.fisher$p.value < 0.975){ # 
+          independence.betterScore = independence.betterScore + 1 
+        }else if((pre.score - pro.score < 0) && (res.fisher$p.value <= 0.025  || res.fisher$p.value >= 0.975)){ # 
+          dependence.warseScore = dependence.warseScore + 1
+        }else{                                                        # 
+          dependence.betterScore = dependence.betterScore + 1
         }
       }
     }
     if(data.index == 0){
-      independence.inc = c(tp.val/data.size.num)
-      independence.dec = c(fp.val/data.size.num)
-      dependence.inc = c(fn.val/data.size.num)
-      dependence.dec = c(tn.val/data.size.num)
+      independence.betterScore.vec = c(independence.betterScore/data.size.num)
+      independence.warseScore.vec = c(independence.warseScore/data.size.num)
+      dependence.betterScore.vec = c(dependence.betterScore/data.size.num)
+      dependence.warseScore.vec = c(dependence.warseScore/data.size.num)
     }else{
-      independence.inc = append(independence.inc, tp.val/data.size.num)
-      independence.dec = append(independence.dec, fp.val/data.size.num)
-      dependence.inc = append(dependence.inc, fn.val/data.size.num)
-      dependence.dec = append(dependence.dec, tn.val/data.size.num)
+      independence.betterScore.vec = append(independence.betterScore.vec, independence.betterScore/data.size.num)
+      independence.warseScore.vec = append(independence.warseScore.vec, independence.warseScore/data.size.num)
+      dependence.betterScore.vec = append(dependence.betterScore.vec, dependence.betterScore/data.size.num)
+      dependence.warseScore.vec = append(dependence.warseScore.vec, dependence.warseScore/data.size.num)
     }
     data.index = data.index + 1
-    plotResult(x,y, p.value,"BIC score diff", "p-value",paste("Data size : ", data.size.num))
-    print(paste("Data Size : ", data.size.num))
-    printResultbyTable(tp.val, fp.val, fn.val,tn.val)
-    printResultbyLine(tp.val, fp.val, fn.val,tn.val)
+#    plotResult(x,y, p.value,"BIC score diff", "p-value",paste("Data size : ", data.size.num))
+#    print(paste("Data Size : ", data.size.num))
+#    printResultbyTable(independence.betterScore, independence.warseScore, dependence.betterScore,dependence.warseScore)
+#    printResultbyLine(independence.betterScore, independence.warseScore, dependence.betterScore,dependence.warseScore)
     print(proc.time() - ptm)
   }
-  plotData(data.size.num.vec, independence.inc, "Data Size", "independence inc","red")
-  plotData(data.size.num.vec, independence.dec, "Data Size", "independence dec", "blue")
-  plotData(data.size.num.vec, dependence.inc, "Data Size", "dependence inc", "black")
-  plotData(data.size.num.vec, dependence.dec, "Data Size", "dependence dec", "yellow")
+  plotData(data.size.num.vec, independence.betterScore.vec, "Data Size", "independence and better Score","red")
+  plotData(data.size.num.vec, independence.warseScore.vec, "Data Size", "independence and warse Score", "blue")
+  plotData(data.size.num.vec, dependence.betterScore.vec, "Data Size", "dependence and better Score", "black")
+  plotData(data.size.num.vec, dependence.warseScore.vec, "Data Size", "dependence and warseScore", "yellow")
   par(new=F)
 }
 
 # Program Start
-main(data.size.num = c(10,20))
+test.data = c(10,20,30,40,50,60,70,80,90,100,110,120,130,140)
+#main(data.size.num = test.data)
+independence.betterScore.vec = c(11.90476, 19.19505, 15.92775, 12.14575, 9.650456, 8.237719, 6.905371, 5.556469, 4.806101, 4.089283, 3.473473, 2.970575, 2.514993, 2.170374)
+independence.warseScore.vec  = c(0, 11.14551,23.15271, 32.58562, 39.318281, 44.147774, 48.127648, 51.599034, 54.221408, 56.648114, 58.670820, 60.427509, 62.003868, 63.358212)
+dependence.betterScore.vec   = c(28.57143, 43.13725, 42.69294, 41.12047, 39.94789, 38.493956, 36.748483, 35.933687, 34.729316, 33.564766, 32.632161, 31.921087, 31.021676, 30.429075)
+dependence.warseScore.vec    = c(59.52381, 26.52219, 18.22660, 14.14816, 11.08337, 9.120551, 8.218498, 6.910811, 6.243176, 5.697837, 5.223546, 4.680829, 4.459463, 4.042339)
+
+plotData(test.data, independence.betterScore.vec, "Data Size", "independence and better Score","red")
+plotData(test.data, independence.warseScore.vec, "Data Size", "independence and warse Score", "blue")
+plotData(test.data, dependence.betterScore.vec, "Data Size", "dependence and better Score", "black")
+plotData(test.data, dependence.warseScore.vec, "Data Size", "dependence and warseScore", "yellow")
 
